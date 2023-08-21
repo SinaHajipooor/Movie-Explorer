35e1c40f7db073596bb9fd511524ba53662143f9
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-21 20:42:02 +0330

    implement the component composition for navbar children

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { useState } from "react";
 import Main from "./components/main/main";
 import NavBar from "./components/navBar/navBar";
 import { tempMovieData } from "./data/dummyData";
+import Logo from "./components/navBar/logo";
+import Search from "./components/navBar/search";
+import NumResults from "./components/navBar/numResults";
 
 export default function App() {
     // state 
     const [movies, setMovies] = useState(tempMovieData);
     return (
         <>
-            <NavBar movies={movies} />
+            <NavBar >
+                <Logo />
+                <Search />
+                <NumResults movies={movies} />
+            </NavBar>
             <Main movies={movies} />
         </>
     );
--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,13 +1,8 @@
-import Logo from "./logo";
-import NumResults from "./numResults";
-import Search from "./search";
 
-export default function NavBar({ movies }) {
+export default function NavBar({ children }) {
 
     //UI
     return <nav className="nav-bar">
-        <Logo />
-        <Search />
-        <NumResults movies={movies} />
+        {children}
     </nav>
 }

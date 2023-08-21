cd6f8c167701123ba65b6b2c569e80624e6e4801
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-21 22:05:14 +0330

    make the star widget flexible

--- src/components/elements/starRating.js
+++ src/components/elements/starRating.js
@@ -12,25 +12,29 @@ const starContainerStyle = {
     display: 'flex',
 };
 
-const textStyle = {
-    lineHeight: '1',
-    margin: '0',
-};
 
 
-export default function StarRating({ maxRating = 5 }) {
+export default function StarRating({ maxRating = 5, color = '#fcc419', size = 48, className = "" }) {
     // states
     const [rating, setRating] = useState(0);
     const [tempRating, setTempRating] = useState(0)
+
+    const textStyle = {
+        lineHeight: '1',
+        margin: '0',
+        color,
+        fontSize: `${size / 1.5}px`,
+    };
+
     // methods
     function handleRating(rating) {
         setRating(rating);
     }
 
     // UI
-    return <div style={containerStyle}>
+    return <div style={containerStyle} className={className}>
         <div style={starContainerStyle}>
-            {Array.from({ length: maxRating }, (_, i) => <Star key={i} onRate={() => handleRating(i + 1)} isFull={tempRating ? tempRating >= i + 1 : rating >= i + 1} onHoverIn={() => setTempRating(i + 1)} onHoverOut={() => setTempRating(0)} />)}
+            {Array.from({ length: maxRating }, (_, i) => <Star key={i} onRate={() => handleRating(i + 1)} isFull={tempRating ? tempRating >= i + 1 : rating >= i + 1} onHoverIn={() => setTempRating(i + 1)} onHoverOut={() => setTempRating(0)} color={color} size={size} />)}
         </div>
         <p style={textStyle}>{tempRating || rating || ''}</p>
 

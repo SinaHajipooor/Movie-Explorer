4fcae28960e9c9bb07be43e157a73448f6b86a78
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-23 10:28:52 +0330

    config the query search for movies

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ const APIKEY = '76e7fb94';
 
 export default function App() {
     // state 
-    const [movies, setMovies] = useState(tempMovieData);
-    const [watched, setWatched] = useState(tempWatchedData);
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
-    const [query, setQuery] = useState();
+    const [query, setQuery] = useState('');
     // lifecycles
     useEffect(() => {
         async function fetchMovies() {
             try {
                 setIsLoading(true);
-                const response = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=interstell ar`);
+                setError('');
+                const response = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`);
                 if (!response.ok) throw Error('failed to fetch all the movies');
                 const data = await response.json();
                 if (data.response === 'False') throw new Error('movie not found');
@@ -37,8 +38,13 @@ export default function App() {
                 setIsLoading(false)
             }
         }
+        if (query.length < 3) {
+            setMovies([]);
+            setError('');
+            return;
+        }
         fetchMovies();
-    }, [])
+    }, [query])
 
     // UI
     return (

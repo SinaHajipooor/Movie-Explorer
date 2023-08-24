f18fa4dab896be0de5a0df5fd1063e396e1b614c
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-24 10:03:37 +0330

    add the eventListener on the effect

--- src/components/navBar/search.js
+++ src/components/navBar/search.js
@@ -1,11 +1,31 @@
+import { useEffect, useRef } from "react"
 
 export default function Search({ query, setQuery }) {
-    // UI
+    // ------------- states ----------------
+    const inputEl = useRef()
+    // ------------- lifecycle ----------------
+    useEffect(() => {
+        function callBack(e) {
+            if (document.activeElement === inputEl) return;
+
+            if (e.code === 'Enter') {
+                inputEl.current.focus();
+                setQuery('');
+            }
+        }
+        // add the events
+        document.addEventListener('keydown', callBack);
+        // remove the event listener
+        return () => document.removeEventListener('keydown', callBack)
+
+    }, [setQuery])
+    // ------------- UI ----------------
     return <input
         className="search"
         type="text"
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
     />
 }

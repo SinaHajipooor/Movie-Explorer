4dd0443532866724c4fbe11cf972852f4f20cb4f
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-24 13:36:44 +0330

    optimization

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorageState(initialState, key) {
-  const [value, setValue] = useState(function () {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialState;
-  });
+    // get the items
+    const [value, setValue] = useState(function () {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : initialState;
+    });
 
-  useEffect(
-    function () {
-      localStorage.setItem(key, JSON.stringify(value));
-    },
-    [value, key]
-  );
+    // set the items
+    useEffect(
+        function () {
+            localStorage.setItem(key, JSON.stringify(value));
+        },
+        [value, key]
+    );
 
-  return [value, setValue];
+    return [value, setValue];
 }
+
+
+

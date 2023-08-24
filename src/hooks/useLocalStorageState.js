4f5c2cafeaec31a6d21b7fbd8d6b18a56820ddad
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-24 13:14:55 +0330

    debug the custom hooks

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,20 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export function useLocalStorageState({ initialState, key }) {
-    // get the items
-    const [value, setValue] = useState(function () {
-        const storedValue = localStorage.getItem(key);
-        return storedValue ? JSON.parse(storedValue) : initialState;
-    });
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
 
-    // set the items
-    // set the watched movies into local storage
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value))
-    }, [value, key]);
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
 
-
-    return [value, setValue];
-
-
+  return [value, setValue];
 }

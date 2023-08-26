553647dce94d731fb0a896ddfdfe0764007c8a1e
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-26 18:43:09 +0330

    coinditional rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css'; 
+// import './index.css';  // for the usePopCorn app
 import './quiz/index.css' // for the quiz app
-// import App from './App';
+// import App from './App';  // for the usePopCorn app
 import App from './quiz/App' // for the quiz app
 
 

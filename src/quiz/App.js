4124d8ba1c5bbe32e921c5c64e623e41e4899de6
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-26 11:52:00 +0330

    fetch the questions list

--- src/quiz/App.js
+++ src/quiz/App.js
@@ -1,8 +1,13 @@
 
+import { useEffect } from 'react';
 import Header from './Header';
 import Main from './Main'
 
 export default function App() {
+    // fetch the questions list
+    useEffect(function () {
+        fetch('http://localhost:9000/questions').then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err.message));
+    }, [])
     return <div className="app">
         <Header />
         <Main className='main'>

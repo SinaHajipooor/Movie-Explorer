7fedb7120ca224d68ad4975e7a3490f81a421efc
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-26 18:38:51 +0330

    config the rendering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-import './quiz/index.css'
+// import './index.css'; 
+import './quiz/index.css' // for the quiz app
 // import App from './App';
-import App from './quiz/App'
+import App from './quiz/App' // for the quiz app
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

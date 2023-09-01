9c5b70cb24cf69725d693ebad89a7b5facbbe6c2
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 18:46:03 +0330

    connect redux store

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';  // for the usePopCorn app
 // import App from './quiz/App' // for the quiz app
 import App from './redux/App'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
 

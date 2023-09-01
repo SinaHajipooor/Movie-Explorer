70481557210c0e9d6dae4f2016682edc5310d28c
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 18:48:21 +0330

    add each feature components

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 // import './quiz/index.css' // for the quiz app
 // import App from './App';  // for the usePopCorn app
 // import App from './quiz/App' // for the quiz app
-import App from './redux/App'
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import App from './redux/App' // for the redux test app
+import { Provider } from 'react-redux';// for the redux test app
+import store from './redux/store';// for the redux test app
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

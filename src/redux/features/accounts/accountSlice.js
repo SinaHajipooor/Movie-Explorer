95c30a33bcc146f7792df72ca6a37a8f9f4eacea
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 18:42:41 +0330

    professional redux file structure

---
+++ src/redux/features/accounts/accountSlice.js
@@ -0,0 +1,42 @@
+
+const initialStateAccount = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: '',
+};
+
+
+export default function accountReducer(state = initialStateAccount, action) {
+    switch (action.type) {
+        case 'account/deposit':
+            return { ...state, balance: state.balance + action.payload }
+        case 'account/withdraw':
+            return { ...state, balance: state.balance - action.payload }
+
+        case 'account/requestLoadn':
+            // he can only request a loan if he didnt reuqest it in the pas so we check if the loan is grater than zero or not 
+            if (state.loan > 0) return state;
+            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount }
+        case 'account/payLoan':
+            return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
+
+        // here instead of throwing new Error , we just return the original state
+        default:
+    }
+}
+
+
+
+// account action creators
+export function deposit(amount) {
+    return { type: 'account/deposit', payload: amount };
+}
+export function withdraw(amount) {
+    return { type: 'account/withdraw', payload: amount }
+}
+export function requestLoan(amount, purpose) {
+    return { type: 'account/requestLoan', payload: { amount, purpose } }
+}
+export function payLoan() {
+    return { type: 'account/payLoan' }
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,47 +1,6 @@
 import { combineReducers, createStore } from "redux";
-
-const initialStateAccount = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: '',
-};
-
-const initilaStateCustomer = {
-    fullName: '',
-    nationalID: '',
-    createdAt: ''
-}
-
-
-function accountReducer(state = initialStateAccount, action) {
-    switch (action.type) {
-        case 'account/deposit':
-            return { ...state, balance: state.balance + action.payload }
-        case 'account/withdraw':
-            return { ...state, balance: state.balance - action.payload }
-
-        case 'account/requestLoadn':
-            // he can only request a loan if he didnt reuqest it in the pas so we check if the loan is grater than zero or not 
-            if (state.loan > 0) return state;
-            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount }
-        case 'account/payLoan':
-            return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
-
-        // here instead of throwing new Error , we just return the original state
-        default:
-    }
-}
-function customerReducer(state = initilaStateCustomer, action) {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return { ...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt }
-        case 'customer/updateName':
-            return { ...state, fullName: action.payload }
-
-        default: return state;
-
-    }
-}
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 
 // combine all the reucers that we have 
@@ -53,27 +12,6 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 
-
-// account action creators
-function deposit(amount) {
-    return { type: 'account/deposit', payload: amount };
-}
-function withdraw(amount) {
-    return { type: 'account/withdraw', payload: amount }
-}
-function requestLoan(amount, purpose) {
-    return { type: 'account/requestLoan', payload: { amount, purpose } }
-}
-function payLoan() {
-    return { type: 'account/payLoan' }
-}
+export default store;
 
 
-// customer action creators 
-function createCustomer(fullName, nationalID) {
-    return { type: 'customer/createCustomer', payload: { fullName, nationalID, createdAt: new Date().toISOString() } }
-}
-
-function updateName(fullName) {
-    return { type: 'customer/updateName', payload: fullName }
-}

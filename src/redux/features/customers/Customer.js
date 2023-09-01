4b62be17f194dc64be418be16425569479e7bfb2
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 19:59:09 +0330

    optimzie

--- src/redux/features/customers/Customer.js
+++ src/redux/features/customers/Customer.js
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 
 function Customer() {
-
+    // states
     // get the customer data from store 
     const customer = useSelector(store => store.customer.fullName);
-
+    // ui
     return <h2>👋 Welcome,{customer}</h2>;
 }
 

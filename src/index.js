d10a7b3855ee0187665c3181b0569bdd5503ecb1
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 17:15:43 +0330

    initial redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';  // for the usePopCorn app
-import './quiz/index.css' // for the quiz app
+// import './quiz/index.css' // for the quiz app
 // import App from './App';  // for the usePopCorn app
-import App from './quiz/App' // for the quiz app
-
+// import App from './quiz/App' // for the quiz app
+import App from './redux/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

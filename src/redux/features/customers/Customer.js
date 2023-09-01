a6f39f2a805a435af8c8c45a5982538c3201574d
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-01 18:58:23 +0330

    read customer from store

--- src/redux/features/customers/Customer.js
+++ src/redux/features/customers/Customer.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+
+    // get the customer data from store 
+    const customer = useSelector(store => store.customer.fullName);
+
+    return <h2>👋 Welcome,{customer}</h2>;
 }
 
 export default Customer;
